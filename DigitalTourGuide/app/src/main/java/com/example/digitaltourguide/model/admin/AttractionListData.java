c379180b6deb7c382fbe95a250e6e322b4ea0380
Palomino <428352832@qq.com>
2026-07-11 16:56:06 +0800

    ui

--- DigitalTourGuide/app/src/main/java/com/example/digitaltourguide/model/admin/AttractionListData.java
+++ DigitalTourGuide/app/src/main/java/com/example/digitaltourguide/model/admin/AttractionListData.java
@@ -1,11 +1,16 @@
 package com.example.digitaltourguide.model.admin;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class AttractionListData {
+    @SerializedName("list")
     private List<AddAttractionRequest> list;
+    @SerializedName("nextLastId")
     private String nextLastId;
+    @SerializedName("hasMore")
     private boolean hasMore;
+
     public List<AddAttractionRequest> getList() { return list; }
     public String getNextLastId() { return nextLastId; }
     public boolean isHasMore() { return hasMore; }
